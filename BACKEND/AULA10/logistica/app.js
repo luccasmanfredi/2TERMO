6aeb8411fc814bc4592ecc979474e448d9a514f1
Luccas Manfredi <manfrediluccas07@yahoo.com>
2026-09-04 11:12:29 -0300

    .

--- BACKEND/AULA10/logistica/app.js
+++ BACKEND/AULA10/logistica/app.js
@@ -3,7 +3,7 @@ const entrada = require(`readline-sync`);
 const logistica = require('./calculadoraFrete.js');
 
 const produto = entrada.question("Informe o nome do produto: ");
-const km = entrada.questionInt("Qual a distancia da entrega em KM? ");
+const  km = entrada.questionInt("Qual a distancia da entrega em KM? ");
 const valorCarga = entrada.questionFloat("Qual o valor total da carga? ");
 
 const freteBase = logistica.calcularBase(km);
@@ -11,7 +11,7 @@ const seguro = logistica.calcularSeguro(valorCarga);
 const prazo = logistica.verificarPrazo(km);
 
 const total = freteBase + seguro;
- 
+
 console.log(`====== RELATORIO DE POSTAGEM ======
 Produto: ${produto}
 Frete base: R$ ${freteBase.toFixed(2)}
@@ -19,5 +19,4 @@ Seguro: R$ ${seguro.toFixed(2)}
 Total: R$ ${total.toFixed(2)}
 Prazo de entrega: ${prazo}
 ===================================`);
-
 
